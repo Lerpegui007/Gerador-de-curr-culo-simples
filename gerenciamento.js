8db0da26f6602cf3112cc90acf036aa529922351
Lerpegui <lucasemanuelribeiro2008@gmail.com>
2026-06-23 22:38:53 -0300

    Add files via upload

---
+++ gerenciamento.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formularioCurriculo')
+
+formulario.addEventListener('submit',function(event){
+event.preventDefault();
+
+const formData = new FormData(formulario)
+const nome = formData.get('nome')
+const idade = formData.get('idade')
+const email = formData.get('email')
+const telefone = formData.get('telefone')
+const cidade = formData.get('cidade')
+const endereco = formData.get('endereço')
+const perfil_profissional = formData.get('perfil profissional')
+const habilidades = formData.get('habilidades')
+const formacao = formData.get('formação')
+const experiencia_profissional = formData.get('experiência profissional')
+
+localStorage.setItem('nomeFor',nome)
+localStorage.setItem('idadeFor',idade)
+localStorage.setItem('emailFor',email)
+localStorage.setItem('telefoneFor',telefone)
+localStorage.setItem('cidadeFor',cidade)
+localStorage.setItem('enderecoFor',endereco)
+localStorage.setItem('perfilFor', perfil_profissional)
+localStorage.setItem('habilidadesFor',habilidades)
+localStorage.setItem('formacaooFor',formacao)
+localStorage.setItem('experienciaFor',experiencia_profissional)
+
+window.location.href = 'curriculo.html'
+
+})
